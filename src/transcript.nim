e3b49e8f00b2516e444900553e12b7324ffc1cfd
Mateusz Czapliński <czapkofan@gmail.com>
2019-02-08 18:44:14 +0100

    initial impl fails because 'tag effects differ'

--- src/transcript.nim
+++ src/transcript.nim
@@ -1,7 +1,115 @@
-# This is just an example to get you started. A typical library package
-# exports the main API in this file. Note that you cannot rename this file
-# but you can remove it if you wish.
+{.experimental: "codeReordering".}
+import streams
+import strutils
 
-proc add*(x, y: int): int =
-  ## Adds two files together.
-  return x + y
+type
+  TranscriptStream* = ref object of Stream
+    script: Stream
+    dir: Direction
+    unfetchB: char
+    unfetchDir: Direction
+  TranscriptError* = object of CatchableError
+  # TODO(akavel): use variant type instead of (Direction, char) pair - here and in `unfetch`
+  Direction = enum
+    EOF
+    Read
+    Write
+
+proc transcript*(script: string): TranscriptStream =
+  return transcript(newStringStream(script))
+
+proc transcript*(script: Stream): TranscriptStream =
+  result = new(TranscriptStream)
+  result.script = script
+  result.unfetchDir = EOF
+  # result.closeImpl = scriptClose
+  result.readDataImpl = scriptReadData
+  result.writeDataImpl = scriptWriteData
+  result.flushImpl = proc(s: Stream) = discard
+
+proc scriptReadData(s: Stream; buffer: pointer; bufLen: int): int =
+  let t = TranscriptStream(s)
+  var temp = ""
+  for i in 0..<bufLen:
+    let (dir, b) = t.fetchByte()
+    if dir != Read:
+      t.unfetchB = b
+      t.unfetchDir = dir
+      if i > 0:
+        copyMem(buffer, temp[0].addr, i)
+      return i
+    temp.add(b)
+  if bufLen > 0:
+    copyMem(buffer, temp[0].addr, bufLen)
+  return bufLen
+
+proc scriptWriteData(s: Stream; buffer: pointer; bufLen: int) =
+  let t = TranscriptStream(s)
+  var temp = newString(bufLen)
+  copyMem(temp[0].addr, buffer, bufLen)
+  for i in 0..<bufLen:
+    let (dir, b) = t.fetchByte()
+    if dir != Write:
+      raise newException(TranscriptError, "transcript has $#, but a write was attempted" % $dir)
+    if b != temp[i]:
+      raise newException(TranscriptError, "transcript has Write of 0x$#, but a write of 0x$# was attempted" % [toHex($b), toHex($temp[i])])
+
+# proc scriptClose(s: Stream) =
+#   let t = TranscriptStream(s)
+#   let (dir, b) = t.fetchByte()
+#   if dir != EOF:
+#     raise newException(Exception, "transcript has $# of 0x$#, but a close was attempted" % [$dir, toHex($b)])
+
+proc fetchByte(t: TranscriptStream): (Direction, char) =
+  if t.unfetchDir != EOF:
+    result = (t.unfetchDir, t.unfetchB)
+    t.unfetchDir = EOF
+    return
+  var buf = ""
+  while true:
+    case (let b = t.script.readChar(); b)
+    of hexDigit:
+      if buf == "":
+        buf.add(b)
+        continue
+      elif buf.len == 1 and buf[0] in hexDigit:
+        return (t.dir, chr(buf[0].nibble * 0x10 + buf[1].nibble))
+      else:
+        raise newException(CatchableError, "unexpected '$#' character in transcript" % $buf[0])
+    of '#':
+      discard t.script.readLine()
+    of ' ', '\n', '\r', '\t':
+      discard
+    of '-', '<', '>':
+      if buf == "" and b in {'<', '-'}:
+        buf.add(b)
+        continue
+      elif buf == "<" and b == '-':
+        buf = ""
+        t.dir = Read
+        continue
+      elif buf == "-" and b == '>':
+        buf = ""
+        t.dir = Write
+        continue
+      elif buf.len == 1:
+        raise newException(CatchableError, "unexpected '$#' character in transcript" % $buf[0])
+      else:
+        raise newException(CatchableError, "unexpected '$#' character in transcript" % $b)
+    of '\0':  # EOF
+      if buf.len == 1:
+        raise newException(CatchableError, "unexpected '$#' character in transcript" % $buf[0])
+      t.dir = EOF
+      return (EOF, '\0')
+    else:
+      raise newException(CatchableError, "unexpected '$#' character in transcript" % $b)
+
+const hexDigit = {'0'..'9', 'a'..'f', 'A'..'F'}
+
+func nibble(ch: char): int =
+  case ch
+  of '0'..'9': return ord(ch) - ord('0')
+  of 'a'..'f': return ord(ch) - ord('a') + 10
+  of 'A'..'F': return ord(ch) - ord('A') + 10
+  else:        raise newException(Defect, "nibble(0x$#)" % ch.int.toHex)
+
--- tests/test1.nim
+++ tests/test1.nim
@@ -1,12 +1,42 @@
-# This is just an example to get you started. You may wish to put all of your
-# tests into a single file, or separate them into multiple `test1`, `test2`
-# etc. files (better names are recommended, just make sure the name starts with
-# the letter 't').
-#
-# To run these tests, simply execute `nimble test`.
-
+{.experimental: "codeReordering".}
 import unittest
-
 import transcript
-test "can add":
-  check add(5, 5) == 10
+
+test "simple session, arrow-style, with comments":
+  let session = transcript"""
+# simple comment -> foobar 00 01 02 03
+<-
+2f 6e 69 78 2f 73 74 6f 72 65 2f 67 32 79 6b 35   # /nix/store/g2yk5 |
+34 68 69 66 71 6c 73 6a 69 68 61 33 73 7a 72 34   # 4hifqlsjiha3szr4 |
+71 33 63 63 6d 64 7a 79 72 64 76 2d 67 6c 69 62   # q3ccmdzyrdv-glib |
+63 2d 32 2e 32 37 00 00                           # c-2.27..         |
+
+# 1463  write(1, "\313\356RT\0\0\0\0\4\2\0\0\0\0\0\0", 16) = 16
+<- # 0s  16 bytes
+cb ee 52 54 00 00 00 00 04 02 00 00 00 00 00 00   # ..RT............ |
+
+# 1463  read(0, "\353\235\f9\0\0\0\0\4\2\0\0\0\0\0\0", 8192) = 16
+-> # 0s  16 bytes
+eb 9d 0c 39 00 00 00 00 04 02 00 00 00 00 00 00   # ...9............ |
+"""
+  check session.readAll().toHex == strip_space"""
+2f 6e 69 78 2f 73 74 6f 72 65 2f 67 32 79 6b 35
+34 68 69 66 71 6c 73 6a 69 68 61 33 73 7a 72 34
+71 33 63 63 6d 64 7a 79 72 64 76 2d 67 6c 69 62
+63 2d 32 2e 32 37 00 00"""
+  session.write(strip_space"""
+cb ee 52 54 00 00 00 00 04 02 00 00 00 00 00 00""".parseHex)
+  check session.readAll().toHex == strip_space"""
+eb 9d 0c 39 00 00 00 00 04 02 00 00 00 00 00 00"""
+
+test "partial read & write":
+  skip
+test "exception on bad byte write":
+  skip
+test "exception on write instead of read":
+  skip
+test "read EOFs when ended":
+  skip
+
+proc strip_space(s: string): string =
+  return s.multiReplace(("\n", ""), (" ", ""))
